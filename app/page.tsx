02d91bea339adf121ab07ffcb0d177bf1676a9a7
Jesus Ruelas <jruelasperez711@gmail.com>
2025-06-12 20:35:06 -0700

    Created Root Login Page. Added Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
+'use client'
+
 import LoginForm from "@/components/LoginForm";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 
+
   return (
-    <div className="">
-
-      <main>
-        <h1>Welcome to Mixed Bevs Reviews!</h1>
-        <p>Learn new mixed bevs and Leave your rating!</p>
-
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <main className="flex-1 flex flex-col justify-center items-center gap-y-10">
+        <div>
+          <h1 className="text-6xl text-primary font-bold">Welcome to Mixed Bevs Reviews!</h1>
+          <p className="text-3xl text-secondary text-center font-semibold">Learn new mixed bevs and Leave your rating!</p>
+        </div>
         <LoginForm />
       </main>
 
-      <footer className="">
+      <footer className="bg-slate-900 py-6 w-full flex flex-col justify-center items-center text-lg font-bold">
+        <div className="flex gap-x-2">
+          <Link className="text-secondary" href={"/"}>Link to Source Code </Link>
+        </div>
       </footer>
     </div>
   );
