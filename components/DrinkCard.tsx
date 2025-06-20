41dd9b332e90c358ff74d5f7970d5d4f81201d64
Jesus Ruelas <jruelasperez711@gmail.com>
2025-06-19 18:38:37 -0700

    Modified component to display Drink image, name, alcoholic, category, etc.

--- components/DrinkCard.tsx
+++ components/DrinkCard.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Card, CardDescription, CardHeader, CardTitle, CardAction, CardContent, CardFooter } from './ui/card'
 import { Button } from './ui/button'
+import { Drink } from '@/types'
+import Image from 'next/image'
 
 
-const DrinkCard = () => {
-  return (
-<Card className="min-w-[300px] max-w-sm w-full border-white border-2 text-white hover:border-blue-400 h-full flex flex-col justify-between whitespace-wrap text-wrap">
+const DrinkCard = ({ idDrink, name, category, alcoholic, glass, instructions, image, tags, dateModified, DrinkIngredients }: Drink) => {
+
+
+  if (!idDrink) {
+    return (
+      <Card className="min-w-[300px] max-w-sm w-full border-white border-2 text-white hover:border-blue-400 h-full flex flex-col justify-between whitespace-wrap text-wrap">
       <CardHeader>
         <CardTitle>Drink Name</CardTitle>
         <CardDescription>This Drink....</CardDescription>
@@ -15,6 +20,25 @@ const DrinkCard = () => {
         <p>Card Content</p>
       </CardContent>
       <CardFooter>
+        <p>Rating: 0/5</p>
+      </CardFooter>
+    </Card>
+    )
+  }
+
+
+  return (
+    <Card className="min-w-[300px] max-w-sm w-full border-white border-2 text-white hover:border-blue-400 h-full flex flex-col justify-between whitespace-wrap text-wrap">
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{alcoholic}</CardDescription>
+        <CardAction><Button>View More</Button></CardAction>
+      </CardHeader>
+      <CardContent className='flex justify-center items-center'>
+        <Image priority src={`/cocktail_images/${idDrink}.jpg`} alt={name} width={300} height={300}/>
+      </CardContent>
+      <CardFooter className='flex flex-col'>
+        <p>{category}</p>
         <p>Rating: 0/5</p>
       </CardFooter>
     </Card>
