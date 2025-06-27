4e35197e905f2b677ea9acfce947ac40d087b6ea
Jesus Ruelas <jruelasperez711@gmail.com>
2025-06-26 18:05:06 -0700

    Removed empty component if id is undefined. Added element to display unrated or the drinks rating out of 5 if the drink has at least 1 rating

--- components/DrinkCard.tsx
+++ components/DrinkCard.tsx
@@ -1,32 +1,12 @@
-import React from 'react'
 import { Card, CardDescription, CardHeader, CardTitle, CardAction, CardContent, CardFooter } from './ui/card'
 import { Button } from './ui/button'
 import { Drink } from '@/types'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
-
-const DrinkCard = ({ idDrink, name, category, alcoholic, glass, instructions, image, tags, dateModified, DrinkIngredients }: Drink) => {
+const DrinkCard = ({ idDrink, name, category, alcoholic, glass, instructions, image, tags, dateModified, DrinkIngredients, avg_rating, rating_count }: Drink) => {
 
   const router = useRouter();
-
-  if (!idDrink) {
-    return (
-      <Card className="min-w-[300px] max-w-sm w-full border-white border-2 text-white hover:border-blue-400 h-full flex flex-col justify-between whitespace-wrap text-wrap">
-      <CardHeader>
-        <CardTitle>Drink Name</CardTitle>
-        <CardDescription>This Drink....</CardDescription>
-        <CardAction><Button>View More</Button></CardAction>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Rating: 0/5</p>
-      </CardFooter>
-    </Card>
-    )
-  }
 
   const handleNav = () => {
     router.push(`/drink/${idDrink}`)
@@ -41,11 +21,11 @@ const DrinkCard = ({ idDrink, name, category, alcoholic, glass, instructions, im
         <CardAction><Button>View More</Button></CardAction>
       </CardHeader>
       <CardContent className='flex justify-center items-center'>
-        <Image priority src={`/cocktail_images/${idDrink}.jpg`} alt={name} width={300} height={300}/>
+        <Image priority src={`/cocktail_images/${idDrink}.jpg`} alt={name} width={300} height={300} />
       </CardContent>
-      <CardFooter className='flex flex-col'>
+      <CardFooter className='flex flex-col gap-y-2'>
         <p>{category}</p>
-        <p>Rating: 0/5</p>
+        <p>{rating_count == 0 ? "Unrated" : `Rating: ${avg_rating} / 5(${rating_count})`}</p>
       </CardFooter>
     </Card>
   )
