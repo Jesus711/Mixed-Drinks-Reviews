fb6bceabd2430741ab23ae67007a3b3cfbca7bb2
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-03 14:24:11 -0700

    Created Timer component to avoid whole page re-renders. Modified supabase table names

--- app/(main)/random/page.tsx
+++ app/(main)/random/page.tsx
@@ -8,6 +8,7 @@ import DrinkCard from "@/components/DrinkCard";
 import CardSkeleton from "@/components/CardSkeleton";
 import DrinkFullView from "@/components/DrinkFullView";
 import FullViewCardSkeleton from "@/components/FullViewCardSkeleton";
+import CountdownTimer from "@/components/CountdownTimer";
 
 type TimeRemaining = {
     hours: number;
@@ -47,12 +48,12 @@ const RandomBev = () => {
                 const today = new Date();
 
                 if (lastDate != null && today.toLocaleDateString() == lastDate){
-                    const { data: lastDrink, error} = await supabase.from("Drinks").select("*, DrinkIngredients(*)").eq("idDrink", lastItemId).single();
+                    const { data: lastDrink, error} = await supabase.from("drinks").select("*, DrinkIngredients(*)").eq("idDrink", lastItemId).single();
                     setDrink(lastDrink)
                     return;
                 }
 
-                const { data: ids } = await supabase.from('Drinks').select('idDrink');
+                const { data: ids } = await supabase.from('drinks').select('idDrink');
                 if (ids === null){
                     throw new Error("No Ids retrieved!")
                 }
@@ -68,7 +69,7 @@ const RandomBev = () => {
                 window.localStorage.setItem("lastItemId", randomId);
                 window.localStorage.setItem("lastVisitDate", today.toLocaleDateString());
 
-                const { data, error } = await supabase.from("Drinks").select("*, DrinkIngredients(*)").eq("idDrink", randomId).single();
+                const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").eq("idDrink", randomId).single();
 
                 if (error) {
                     console.log("Random: Error-", error.message);
@@ -85,13 +86,7 @@ const RandomBev = () => {
             }
         }
 
-        const interval = setInterval(() => {
-            setTimeRemaining(getTimeUntilMidnight());
-        }, 1000); // Update every second
-
         getRandomDrink();
-
-        return () => clearInterval(interval); // Cleanup on unmount
 
     }, [])
 
@@ -108,8 +103,7 @@ const RandomBev = () => {
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-5">
-            <h2 className="text-3xl text-blue-200 font-bold text-center">Next Random Bev in {timeRemaining.hours}hr {timeRemaining.minutes}m {timeRemaining.seconds}s</h2>
-
+            <CountdownTimer />
             { drink !== null ? <DrinkFullView {...drink} /> : (
                 <Loading message="Error" />
             )}
---
+++ components/CountdownTimer.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+type TimeRemaining = {
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+const CountdownTimer = () => {
+    const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(getTimeUntilMidnight());
+
+    function getTimeUntilMidnight() {
+        const now = new Date();
+        const midnight = new Date();
+        midnight.setHours(24, 0, 0, 0); // Set time to midnight
+
+        const diffMs = midnight.getTime() - now.getTime();
+        const hours = Math.floor(diffMs / (1000 * 60 * 60));
+        const minutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diffMs % (1000 * 60)) / 1000);
+
+        return { hours, minutes, seconds };
+    }
+
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setTimeRemaining(getTimeUntilMidnight());
+        }, 1000); // Update every second
+
+
+        return () => clearInterval(interval); // Cleanup on unmount
+
+    }, [])
+
+  return (
+    <h2 className="text-3xl text-blue-200 font-bold text-center">Next Random Bev in {timeRemaining.hours}hr {timeRemaining.minutes}m {timeRemaining.seconds}s</h2>
+  )
+}
+
+export default CountdownTimer
