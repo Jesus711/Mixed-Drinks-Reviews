547e09ba2d81a018b3fdf7709d04379d69bbdd1b
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-03 18:19:43 -0700

    Added mobile responsiveness to Header Layout. Navbar converts to a hamburger menu, click on button to display all nav links

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,7 +10,7 @@ import { supabase } from "@/lib/supabaseClient";
 
 
 export default function HomeLayout({children,}: {children: React.ReactNode}) {
-
+    const [displayMenu, setDisplayMenu] = useState<boolean>(false);
 
     const path = usePathname();
     const router = useRouter();
@@ -25,19 +25,31 @@ export default function HomeLayout({children,}: {children: React.ReactNode}) {
         <div className="min-h-screen flex flex-col">
 
             <header className="bg-gray-900 flex justify-between items-center bg-primary text-white p-4 shadow">
-                <div className="flex flex-col w-full max-w-sm">
-                    <h1 className="text-3xl text-primary font-bold">Mixed Bevs Reviews</h1>
-                    <p className="text-xl text-white">Learn and Rate Mixed Beverages!</p>
+                <div className="flex flex-col">
+                    <h1 className="xl:text-3xl lg:text-xl text-lg text-primary font-bold">Mixed Bevs Reviews</h1>
+                    <p className="xl:text-xl lg:text-md text-xs text-white">Learn and Rate Mixed Beverages!</p>
                 </div>
 
-                <nav className="flex-1 flex justify-center items-center gap-x-14 text-2xl font-semibold">
+                <nav className="flex-1 md:flex hidden justify-center items-center gap-x-14 xl:text-2xl lg:text-lg text-md font-semibold">
                     <Link href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>
                     <Link href={"/random"} className={`${path === "/random" ? "text-primary" : ""}`}>Random Bev</Link>
                     <Link href={"/browse"} className={`${path === "/browse" ? "text-primary" : ""}`}>Browse</Link>
                     <Link href={"/top"} className={`${path === "/top" ? "text-primary" : ""}`}>Top Rated</Link>
                 </nav>
 
-                <Button onClick={handleLogOut} className="bg-red-500 font-semibold text-lg" size={"lg"} variant={"destructive"} type="button">Logout</Button>
+                <Button onClick={handleLogOut} className="md:flex hidden bg-red-500 font-semibold xl:text-lg lg:px-5 px-2 lg:py-3 py-2 text-md hover:cursor-pointer" variant={"destructive"} type="button">Logout</Button>
+                <Button onClick={() => setDisplayMenu(prev => !prev)} variant={"default"} className="md:hidden flex flex-col p-0 justify-center items-center cursor-pointer">
+                    <div className="bg-white w-8 h-1 rounded-md" />
+                    <div className="bg-white w-8 h-1 rounded-md" />
+                    <div className="bg-white w-8 h-1 rounded-md" />
+                </Button>
+                
+                <nav className={`md:hidden absolute right-3.5 top-15 rounded-lg z-10 p-4 bg-gray-900 border-2 border-blue-300 flex sm:w-[185px] flex-col gap-y-3 text-2xl font-semibold ${displayMenu ? "flex" : "hidden"}`}>
+                    <Link onClick={() => setDisplayMenu(false)} href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>
+                    <Link onClick={() => setDisplayMenu(false)} href={"/random"} className={`${path === "/random" ? "text-primary" : ""}`}>Random Bev</Link>
+                    <Link onClick={() => setDisplayMenu(false)} href={"/browse"} className={`${path === "/browse" ? "text-primary" : ""}`}>Browse</Link>
+                    <Link onClick={() => setDisplayMenu(false)} href={"/top"} className={`${path === "/top" ? "text-primary" : ""}`}>Top Rated</Link>
+                </nav>
             </header>
 
             <main className="p-6">{children}</main>
