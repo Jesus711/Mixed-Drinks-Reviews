f6b21f8c27c53b42f0fb3fd313eef076ffe26b39
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-15 20:14:03 -0700

    Implemented drink recommendations to be based on users last viewed drink and any drink rated by the user. Currently retrieves the last view drink and the first rated drink by the user. Modified the amount of skeleton cards to appear. Removed setTimeout function calls.

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -6,6 +6,10 @@ import DrinkCard from '@/components/DrinkCard'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { Drink } from '@/types'
 import CardSkeleton from '@/components/CardSkeleton'
+import { User } from '@supabase/supabase-js'
+
+const SKELETON_CARDS = 7;
+
 
 const HomePage = () => {
 
@@ -18,58 +22,110 @@ const HomePage = () => {
 
   const router = useRouter();
 
+  const ratingSort = (a: Drink, b: Drink) => {
+    if (a.avg_rating > b.avg_rating){
+      return -1
+    }
+
+    return 1
+  }
+
   useEffect(() => {
     const getUser = async () => {
       const { data: { user } } = await supabase.auth.getUser();
 
       if (!user) {
         router.push("/")
+        return
       }
 
-      const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").limit(5);
-      if (error) {
-        console.log("ERROR", error.message);
-      }
-      else {
-        console.log("Drinks", data);
-        setDrinks(data);
-      }
 
-      getLastViewed()
-      getUserRatedDrinks()
+      let prevDrinks = getLastViewed()
 
-      console.log("HELLO", user)
+      let userRatedDrinks = await getUserRatedDrinks(user)
+
+      getDrinkRecommendations(prevDrinks, userRatedDrinks)
+
       setUserName(user?.user_metadata.display_name);
-      setTimeout(() => setLoading(false), 500);
+      setLoading(false)
     }
 
-    const getDrinkRecommendations = async () => {
+    const getDrinkRecommendations = async (prevDrinks: Drink[], userRatedDrinks: Drink[]) => {
 
-      const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").limit(5);
-      if (error) {
-        console.log("ERROR", error.message);
+      // If user has no previous viewed drinks or has not rated any drinks, recommend the top rated
+      // else, using the most recently viewed drink and one of the rated drinks, retrieve 10 drinks with similar ingredients
+      // if the number of recommended drinks is less than 7, append the 5 top rated drinks
+      if (!prevDrinks && !userRatedDrinks) {
+        const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").order("avg_rating", {ascending: false}).limit(10);
+        if (error) {
+          console.log("ERROR", error.message);
+        }
+        else {
+          setDrinks(data);
+        }
+      } else {
+          let ingredients: string[] = []
+          let ids: number[] = []
+
+          if (prevDrinks.length > 0) {
+            ingredients = [...prevDrinks[0].DrinkIngredients.map(ingred => ingred.ingredient)]
+            ids = [...prevDrinks.map(drink => drink.idDrink)]
+          }
+
+          if (userRatedDrinks.length > 0) {
+            ingredients = [...ingredients, ...userRatedDrinks[0].DrinkIngredients.map(ingred => ingred.ingredient)]
+            ids = [...ids, ...userRatedDrinks.map(drink => drink.idDrink)]
+          }
+
+          console.log(ids)
+          const { data, error } = await supabase.from("DrinkIngredients").select("id, drinks(*, DrinkIngredients(*))").in("ingredient", ingredients).not("idDrink", "in", `(${ids.join(",")})`).limit(10);
+          if (error) {
+            console.log("ERROR", error.message);
+          }
+          else {
+            let drinks: Drink[] = []
+            for(let i = 0; i < data.length; i++){
+              let drink: Drink | Drink[] = data[i].drinks
+              if(Array.isArray(drink)){
+                drink = drink[0]
+              }
+              drinks.push(drink)
+            }
+
+            if(drinks.length < 7) {
+              const { data: highRated, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").order("avg_rating", {ascending: false}).limit(5);
+              if (error) {
+                console.log("ERROR", error.message);
+              }
+              else {
+                drinks = [...drinks, ...highRated]
+              }
+            }
+
+            drinks.sort(ratingSort)
+            setDrinks(drinks);
+          }
       }
-      else {
-        console.log("Drinks", data);
-        setDrinks(data);
-      }
+
+
     }
 
-    const getUserRatedDrinks = async () => {
+    const getUserRatedDrinks = async (user: User) => {
    
       const { data: rated, error: ratedError } = await supabase
         .from('drink_ratings')
-        .select('*, drinks(*)')
-        .eq('user_id', (await supabase.auth.getUser()).data.user?.id);
+        .select('*, drinks(*, DrinkIngredients(*))')
+        .eq('user_id', user.id);
 
       if (ratedError) {
         console.error('Error fetching rated drinks:', ratedError);
       } else {
         const drinks = rated.map(r => r.drinks);
-        console.log(drinks)
         setRatedDrinks(drinks)
+        return drinks
       }
 
+      return []
     }
 
     const getLastViewed = () => {
@@ -77,7 +133,10 @@ const HomePage = () => {
       if (stored !== null) {
         const prev = JSON.parse(stored)
         setPrevViewedDrinks(prev)
+        return prev
       }
+
+      return []
     }
 
     getUser();
@@ -92,9 +151,9 @@ const HomePage = () => {
 
       <section className='flex-1 flex flex-col gap-y-1.5'>
         <h2 className='text-primary md:text-3xl text-xl font-semibold md:text-left text-center'>Drink Recommendations: </h2>
-          <ScrollArea className='w-full rounded-2xl border-4 border-orange-400 whitespace-nowrap'>
+          <ScrollArea className=' rounded-2xl border-4 border-orange-400 whitespace-nowrap'>
             <div className='md:p-6 p-3 flex items-center space-x-6 overflow-x-auto scroll-smooth'>
-              {loading ? Array(5).fill(0).map((_, index) => (
+              {loading ? Array(SKELETON_CARDS).fill(0).map((_, index) => (
                 <CardSkeleton key={index} />
               )) 
               : 
@@ -112,7 +171,7 @@ const HomePage = () => {
           <h2 className='text-primary md:text-3xl text-xl font-semibold md:text-left text-center'>Previously Viewed:</h2>
           <ScrollArea className='w-full rounded-2xl border-4 border-orange-400 whitespace-nowrap'>
             <div className='md:p-6 p-3 flex items-center space-x-6 overflow-x-auto scroll-smooth'>
-              {loading ? Array(5).fill(0).map((_, index) => (
+              {loading ? Array(SKELETON_CARDS).fill(0).map((_, index) => (
                 <CardSkeleton key={index} />
               )) 
               : 
@@ -131,7 +190,7 @@ const HomePage = () => {
           <h2 className='text-primary md:text-3xl text-xl font-semibold md:text-left text-center'>Drinks rated by me:</h2>
           <ScrollArea className='w-full rounded-2xl border-4 border-orange-400 whitespace-nowrap'>
             <div className='md:p-6 p-3 flex items-center space-x-6 overflow-x-auto scroll-smooth'>
-              {loading ? Array(5).fill(0).map((_, index) => (
+              {loading ? Array(SKELETON_CARDS).fill(0).map((_, index) => (
                 <CardSkeleton key={index} />
               )) 
               : 
