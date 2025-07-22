d0ace8f598d53324bee30fd89fb90432de18132d
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-21 21:19:23 -0700

    Moved dialog pop up and handleDrankRating function to DrinkFullView. Removed unused props in StarRating component. User is now able to modify their rating. Star rating components are updated after user rates the drink to shown their rating and most updated avg rating

--- components/DrinkFullView.tsx
+++ components/DrinkFullView.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Card, CardDescription, CardHeader, CardTitle, CardAction, CardContent, CardFooter } from './ui/card'
+import { Button } from './ui/button'
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Drink } from '@/types'
 import Image from 'next/image'
 import StarRating from './StarRating'
@@ -9,8 +11,11 @@ import { supabase } from '@/lib/supabaseClient'
 
 const DrinkFullView = ({ idDrink, name, category, alcoholic, glass, instructions, image, tags, dateModified, DrinkIngredients, avg_rating, rating_count }: Drink) => {
   const [userRated, setUserRating] = useState<number>(-1);
+  const [selectedRating, setSelectedRating] = useState<number>(-1);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [avgRating, setAvgRating] = useState<number>(avg_rating);
+  const [ratingCount, setRatingCount] = useState<number>(rating_count);
 
-  console.log(avg_rating, rating_count)
 
   const displayUserRating = async () => {
     const { data: { user } } = await supabase.auth.getUser();
@@ -23,12 +28,66 @@ const DrinkFullView = ({ idDrink, name, category, alcoholic, glass, instructions
 
     const { data, error } = await supabase.from("drink_ratings").select("rating").eq("user_id", user.id).eq('idDrink', idDrink).maybeSingle();
 
-    if (data === null){
+    if (data === null) {
       console.log("User has not rated this drink!")
     }
     else {
       setUserRating(data.rating)
     }
+  }
+
+  const handleStarClicked = (ratingClicked: number) => {
+    setSelectedRating(ratingClicked)
+    setOpenDialog(true);
+  }
+
+  const handleDrinkRating = async (confirmRating: boolean) => {
+
+    if (confirmRating){
+      const { data: { user } } = await supabase.auth.getUser();
+
+      console.log(user?.id, selectedRating, "Rated!", idDrink)
+
+      console.log({
+        user_id: user?.id,
+        idDrink,
+        rating: selectedRating
+      })
+
+      let rating = {
+        user_id: user?.id,
+        idDrink: idDrink,
+        rating: selectedRating
+      }
+
+      const { data, error } = await supabase.from("drink_ratings").upsert(rating, { onConflict: 'user_id, idDrink' });
+
+      console.log(data, error)
+      setUserRating(selectedRating)
+      await updateRatingAvgCount()
+    } else {
+      setSelectedRating(-1)
+    }
+    
+    setOpenDialog(false)
+  }
+
+  const updateRatingAvgCount = async () => {
+    const { data: rating, error} = await supabase.from("drinks").select("avg_rating, rating_count").eq("idDrink", idDrink).single();
+
+    if (error) {
+      console.log(error)
+    }
+    else {
+      setAvgRating(rating.avg_rating);
+      setRatingCount(rating.rating_count);
+    }
+  }
+
+
+  const updateExistingRating = () => {
+    setUserRating(-1)
+    setSelectedRating(-1)
   }
 
   useEffect(() => {
@@ -44,14 +103,18 @@ const DrinkFullView = ({ idDrink, name, category, alcoholic, glass, instructions
           <CardDescription className='text-2xl'>{alcoholic}</CardDescription>
         </div>
 
-        {rating_count === 0 ? (
+        {ratingCount === 0 ? (
           <div className='flex flex-col justify-center items-center bg-gray-700 p-2 rounded-2xl'>
             <h3 className='text-2xl font-bold'>Unrated</h3>
             <p className='text-lg'>Be the First!</p>
           </div>
         ) : (
-          <div>
-            <StarRating idDrink={idDrink} defaultValue={avg_rating} rating_count={rating_count} userRated={false}/>
+          <div className='flex flex-col'>
+            <StarRating rating={avgRating} onRate={handleStarClicked} />
+            <div className='flex text-xl gap-x-1.5 justify-center items-center'>
+              <h3 className='text-amber-400'>{avgRating}/5</h3>
+              <p>({ratingCount})</p>
+            </div>
           </div>
         )}
       </CardHeader>
@@ -60,14 +123,15 @@ const DrinkFullView = ({ idDrink, name, category, alcoholic, glass, instructions
         <div className='flex flex-col gap-y-5'>
           <Image priority className='rounded-md' src={`/cocktail_images/${idDrink}.jpg`} alt={name} width={360} height={360} />
           {userRated != -1 ? (
-            <div>
-              <h3 className='text-center text-2xl font-bold'>Your Rating: {userRated}</h3>
-              <StarRating idDrink={idDrink} rating_count={rating_count} defaultValue={userRated} userRated={true}/>
+            <div className='flex flex-col justify-center items-center gap-y-1'>
+              <h3 className='text-center text-2xl font-bold'>Your Rating: {userRated} Stars</h3>
+              <StarRating rating={userRated} onRate={handleStarClicked} />
+              <Button type='button' onClick={updateExistingRating} variant={"default"} className='bg-blue-500 text-lg px-3 cursor-pointer'>Update Rating</Button>
             </div>
           ) : (
             <div>
               <h3 className='text-center text-2xl font-bold'>Select Your Rating:</h3>
-              <StarRating idDrink={idDrink} rating_count={rating_count} userRated={false} />
+              <StarRating rating={selectedRating} onRate={handleStarClicked} />
             </div>)
           }
         </div>
@@ -111,6 +175,17 @@ const DrinkFullView = ({ idDrink, name, category, alcoholic, glass, instructions
           })}
         </CardDescription>
       </CardFooter>
+      <Dialog open={openDialog}>
+        <DialogContent className="sm:max-w-[360px] bg-orange-100" showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle className='text-center text-xl'>Submit Your Rating: {selectedRating}/5 Stars!</DialogTitle>
+          </DialogHeader>
+          <DialogFooter className='sm:flex sm:justify-center sm:items-center'>
+            <Button type='button' onClick={() => handleDrinkRating(false)} className='bg-red-400 text-lg'>Cancel</Button>
+            <Button type='button' onClick={() => handleDrinkRating(true)} className='bg-orange-400 text-lg'>Rate!</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </Card>
   )
 }
--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -6,77 +6,32 @@ import { supabase } from '@/lib/supabaseClient';
 
 type Props = {
   max?: number;
-  onRate?: (value: number) => void;
-  defaultValue?: number,
-  idDrink: number,
-  rating_count: number,
-  userRated: boolean
+  onRate: (value: number) => void;
+  rating: number,
 };
 
-export default function StarRating({ max = 5, onRate, defaultValue, idDrink, rating_count, userRated }: Props) {
+export default function StarRating({ max = 5, onRate, rating }: Props) {
   const [hoverRating, setHoverRating] = useState(0);
-  const [selectedRating, setSelectedRating] = useState(0);
-  const [openDialog, setOpenDialog] = useState(false);
 
-  const handleDrinkRating = async () => {
-
-    const { data: { user } } = await supabase.auth.getUser();
-
-    console.log(user?.id, selectedRating, "Rated!", idDrink)
-
-    console.log({
-      user_id: user?.id,
-      idDrink,
-      rating: selectedRating
-    })
-
-    let rating = {
-          user_id: user?.id,
-          idDrink: idDrink,
-          rating: selectedRating
-        }
-
-    const { data, error } = await supabase.from("drink_ratings").upsert(rating, {onConflict: 'user_id, idDrink'});
-
-    console.log(data, error)
-    setOpenDialog(false)
-
-  }
-
-  const isReadOnly = typeof defaultValue === 'number';
-
-  const handleCancelRating = () => {
-    setHoverRating(0)
-    setSelectedRating(0)
-    setOpenDialog(false)
-  }
+  const isReadOnly = rating !== -1;
 
   const handleMouseEnter = (value: number) => {
-    if (!selectedRating) setHoverRating(value);
+    if (rating == -1) setHoverRating(value);
   };
 
   const handleMouseLeave = () => {
-    if (!selectedRating) setHoverRating(0);
+    if (rating == -1) setHoverRating(0);
   };
 
   const handleClick = (value: number) => {
-
-    setSelectedRating(value);
-    setOpenDialog(true)
-    if (onRate) onRate(value);
+    onRate(value);
   };
 
   const getIcon = (index: number) => {
-    let rating;
-    if (defaultValue) {
-      rating = defaultValue
-    }
-    else {
-      rating = selectedRating || hoverRating;
-    }
+    let starRating = rating !== -1 ? rating : hoverRating;
 
-    if (rating >= index + 1) return <FaStar className="text-orange-400 w-10 h-10" />;
-    if (rating >= index + 0.5) return <FaStarHalfAlt className="text-orange-400 w-10 h-10" />;
+    if (starRating >= index + 1) return <FaStar className="text-orange-400 w-10 h-10" />;
+    if (starRating >= index + 0.5) return <FaStarHalfAlt className="text-orange-400 w-10 h-10" />;
     return <FaRegStar className="text-gray-400 w-10 h-10" />;
   };
 
@@ -106,29 +61,6 @@ export default function StarRating({ max = 5, onRate, defaultValue, idDrink, rat
           </div>
         ))}
       </div>
-      {userRated &&
-        <div className='flex justify-center items-center'>
-          <Button type='button' onClick={() => console.log("HERE MOVE to DIALOG?")} variant={"default"} className='bg-blue-500 text-lg px-3 cursor-pointer'>Update Rating</Button>
-        </div>
-      }
-
-      {defaultValue && !userRated && <div className='flex text-xl gap-x-1.5 justify-center items-center'>
-        <h3 className='text-amber-400'>{defaultValue}/5</h3>
-        <p>({rating_count})</p>
-      </div>
-      }
-
-      <Dialog open={openDialog}>
-        <DialogContent className="sm:max-w-[360px] bg-orange-100" showCloseButton={false}>
-          <DialogHeader>
-            <DialogTitle className='text-center text-xl'>Submit Your Rating: {selectedRating}/5 Stars!</DialogTitle>
-          </DialogHeader>
-          <DialogFooter className='sm:flex sm:justify-center sm:items-center'>
-            <Button type='button' onClick={handleCancelRating} className='bg-red-400 text-lg'>Cancel</Button>
-            <Button type='button' onClick={handleDrinkRating} className='bg-orange-400 text-lg'>Rate!</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 }
