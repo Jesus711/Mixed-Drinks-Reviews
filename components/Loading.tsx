50f840ecc1a6ca73e0fbd3f5c3c41914c1ce5fcf
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-22 21:25:51 -0700

    Modified skeleton components to shrink on smaller devices

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,10 +4,10 @@ import { Skeleton } from './ui/skeleton'
 const Loading = ({ message = "Loading" }: { message?: string }) => {
     return (
         <div className="flex justify-center items-end gap-x-2">
-            <h2 className="text-5xl font-semibold italic text-blue-200 ">{message}</h2>
-            <Skeleton className="w-3 h-3 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0s' }} />
-            <Skeleton className="w-3 h-3 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0.2s' }} />
-            <Skeleton className="w-3 h-3 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0.4s' }} />
+            <h2 className="lg:text-3xl text-xl font-semibold italic text-blue-200 ">{message}</h2>
+            <Skeleton className="md:w-3 w-2 md:h-3 h-2 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0s' }} />
+            <Skeleton className="md:w-3 w-2 md:h-3 h-2 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0.2s' }} />
+            <Skeleton className="md:w-3 w-2 md:h-3 h-2 rounded-full animate-dot-bounce bg-blue-200" style={{ animationDelay: '0.4s' }} />
         </div>
     )
 }
