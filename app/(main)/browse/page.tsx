3ebd1d4ff3b1e752b7fa06d5567a2be5743e5b1f
Jesus Ruelas <jruelasperez711@gmail.com>
2025-07-22 21:13:57 -0700

    Implement search function. When user searchs, any result found will be displayed and pagination will be shown if more than 1 page of results. Added mobile responsiveness. Added Back to Top button component at bottom of the page.

--- app/(main)/browse/page.tsx
+++ app/(main)/browse/page.tsx
@@ -2,13 +2,17 @@
 import CardSkeleton from '@/components/CardSkeleton';
 import DrinkCard from '@/components/DrinkCard';
 import Loading from '@/components/Loading';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { supabase } from '@/lib/supabaseClient';
 import { Drink } from '@/types'
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react'
 
 const LIMIT = 20; // Amount to retrieve each fetch
+const SKELETON_CARDS = 20;
+
 
 const Browse = () => {
   const [drinkList, setDrinkList] = useState<Drink[]>([]);
@@ -20,6 +24,14 @@ const Browse = () => {
   const [totalPages, setTotalPages] = useState<number>(0);
   const [page, setPage] = useState<number>(1);
   const [pageList, setPageList] = useState<(number | string)[]>([])
+
+
+  const router = useRouter();
+  const params = useSearchParams();
+  const searchParam = typeof params.get("search") === "string" ? params.get("search")! : "";
+
+  console.log("searchParam", searchParam, typeof searchParam, search)
+
 
   const handlePageNav = (index: number) => {
     if (index < 1 || index > totalPages) {
@@ -82,7 +94,7 @@ const Browse = () => {
     const fetchTotalPages = async () => {
       const { count, error } = await supabase
         .from('drinks')
-        .select('*, DrinkIngredients(*)', { count: 'exact', head: true });
+        .select('*, DrinkIngredients(*)', { count: 'exact', head: true }).ilike("name", `%${searchParam}%`);
 
       if (error) {
         console.error('Error fetching count:', error.message);
@@ -95,7 +107,8 @@ const Browse = () => {
     };
 
     fetchTotalPages();
-  }, []);
+  }, [searchParam]);
+
 
   useEffect(() => {
 
@@ -103,7 +116,7 @@ const Browse = () => {
       setLoading(true)
       const start = (page - 1) * LIMIT
       const end = start + LIMIT - 1
-      const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").range(start, end);
+      const { data, error } = await supabase.from("drinks").select("*, DrinkIngredients(*)").ilike("name", `%${searchParam}%`).range(start, end);
 
       if (error) {
         console.log("Error:", error.message);
@@ -111,49 +124,66 @@ const Browse = () => {
       else {
         setDrinkList(data);
       }
-
-      setLoading(false)
+      setTimeout(() => setLoading(false), 250);
     }
 
     getDrinks();
-  }, [page])
+  }, [page, searchParam])
+
+  const handleNavigation = () => {
+    let format_string = search.replace(" ", "_")
+    router.push(`/browse?search=${format_string}`)
+    setPage(1)
+    setSearch("")
+  }
 
 
   return (
-    <div className='flex flex-col justify-center items-center gap-y-7'>
+    <div className='flex flex-col justify-center items-center md:gap-y-7 gap-y-5'>
 
       {/* Search Bar */}
-      <div className='flex justify-center items-center gap-x-3'>
+      <div className='flex md:flex-row flex-col justify-center items-center md:gap-x-3 md:gap-y-0 gap-y-3'>
         <Label htmlFor='search' className='text-3xl text-blue-300'>Search:</Label>
-        <Input type='text' name='search' id='search' className='bg-gray-900 text-blue-300 placeholder:text-gray-400 placeholder:text-xl md:text-xl w-sm h-12' placeholder='Ex: Mojito' />
+        <Input type='text' name='search' id='search' value={search} onChange={(e) => setSearch(e.target.value)} className='bg-gray-900 text-blue-300 placeholder:text-gray-400 placeholder:text-xl md:text-xl xs:w-sm w-[300px] h-12' placeholder='Ex: Mojito' />
+        <Button className='bg-blue-400 text-black text-xl hover:cursor-pointer' type='button' variant={'default'} onClick={handleNavigation}>Search</Button>
       </div>
 
+      {!loading && searchParam !== "" ? drinkList.length === 0 ? (
+        <h1 className='md:text-left text-center md:text-3xl text-xl text-blue-300 font-bold'>No Drinks Found!</h1>
+      ) : (<h1 className='md:text-left text-center md:text-3xl text-xl text-blue-300 font-bold'>Drink Results:</h1>) : <></>}
+
       {/* Drink List */}
-      <div className='flex-1 flex flex-wrap justify-center items-center gap-10'>
+      <div className='flex-1 flex flex-wrap justify-center items-center gap-8'>
         {loading ?
-          Array(18).fill(0).map((_, index) => (
+          Array(SKELETON_CARDS).fill(0).map((_, index) => (
             <CardSkeleton key={index} />
           ))
           : drinkList.map((drink, index) => (
             <DrinkCard {...drink} key={index} />
           ))}
-
       </div>
 
-        {!loading && totalPages > 0 && 
-        <ul className='flex justify-center items-center gap-x-5'>
-          <li onClick={() => handlePageNav(page - 1)} className={`text-2xl font-semibold hover:cursor-pointer text-secondary`}>{'<'}Prev Page</li>
-          {pageList.map((p, index) => {
-            
-            if (typeof p !== 'number')
-              return <li key={index} className={`text-2xl font-semibold hover:cursor-pointer text-secondary`}>{p}</li>
+      
+      {!loading && totalPages > 1 && (
+        <div className='flex flex-col justify-center items-center'>
+          <h2 className='text-blue-300 md:text-2xl text-xl font-bold'>Pages:</h2> 
+          <ul className='flex justify-center items-center gap-x-5'>
+            <li onClick={() => handlePageNav(page - 1)} className={`md:block hidden md:text-2xl text-sm font-semibold hover:cursor-pointer text-secondary`}>{'<'}Prev Page</li>
+            {pageList.map((p, index) => {
 
-            return (
-            <li onClick={() => handlePageNav(p)} key={index} className={`underline text-2xl font-semibold hover:cursor-pointer ${page === p ? 'text-primary' : 'text-secondary'}`}>{p}</li>
-          )})}
-          <li onClick={() => handlePageNav(page + 1)} className={`text-2xl font-semibold hover:cursor-pointer text-secondary`}>Next Page {'>'}</li>
-        </ul>
-        }
+              if (typeof p !== 'number')
+                return <li key={index} className={`md:text-2xl text-lg font-semibold hover:cursor-pointer text-secondary`}>{p}</li>
+
+              return (
+                <li onClick={() => handlePageNav(p)} key={index} className={`underline md:text-2xl text-lg font-semibold hover:cursor-pointer ${page === p ? 'text-primary' : 'text-secondary'}`}>{p}</li>
+              )
+            })}
+            <li onClick={() => handlePageNav(page + 1)} className={`md:block hidden md:text-2xl text-sm font-semibold hover:cursor-pointer text-secondary`}>Next Page{'>'}</li>
+          </ul>
+          <Button variant={'link'} onClick={() => window.scrollTo(0,0)} className='md:text-2xl text-lg font-bold'>Back to Top</Button>
+        </div>
+        )
+      }
     </div>
   )
 }
