c624209aedd51c70515c0f5a85454460a6ddaf22
Jesus Ruelas <jruelasperez711@gmail.com>
2025-11-25 14:09:07 -0800

    Added Link to allow user to continue as guest and browse without logging in or creating an account

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 'use client'
 
 import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 export default function Home() {
 
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center relative overflow-hidden">
       <main className="flex-1 flex flex-col justify-center items-center lg:gap-y-10 gap-y-5">
         <div>
           <h1 className="lg:text-6xl sm:text-4xl xs:text-2xl text-lg text-orange-400 font-bold">Welcome to Mixed Bevs Reviews!</h1>
           <p className="lg:text-3xl sm:text-xl xs:text-lg text-sm text-secondary text-center font-semibold">Learn new mixed bevs and Leave your rating!</p>
         </div>
         <LoginForm />
+        <Link className="sm:text-3xl text-2xl text-blue-200 font-semibold underline hover:text-orange-200" href={"/browse"}>Continue as Guest?</Link>
       </main>
-
     </div>
   );
 }
