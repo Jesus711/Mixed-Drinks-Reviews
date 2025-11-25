4d926cd104fca2e518a7ac81a2d7eef6f91f5102
Jesus Ruelas <jruelasperez711@gmail.com>
2025-11-25 14:12:24 -0800

    Route checks if user is logged in. Displays log in or log out buttons depending on user session status. Changed Mobile menu button to a X button to close when menu is opened

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,12 +6,14 @@ import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
 import { Toaster } from "sonner";
+import { X } from "lucide-react";
 
 
 
 
 export default function HomeLayout({children,}: {children: React.ReactNode}) {
     const [displayMenu, setDisplayMenu] = useState<boolean>(false);
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
     const path = usePathname();
     const router = useRouter();
@@ -25,9 +27,10 @@ export default function HomeLayout({children,}: {children: React.ReactNode}) {
         const { data: { user } } = await supabase.auth.getUser();
 
         if (!user) {
-            router.push("/")
             return
         }
+
+        setLoggedIn(true)
     }
 
     useEffect(() => {
@@ -37,7 +40,6 @@ export default function HomeLayout({children,}: {children: React.ReactNode}) {
     
     return (
         <div className="min-h-screen flex flex-col">
-
             <header className="bg-gray-900 flex justify-between items-center bg-primary text-white p-4 shadow">
                 <div className="flex flex-col">
                     <h1 className="xl:text-3xl lg:text-xl text-lg text-primary font-bold">Mixed Bevs Reviews</h1>
@@ -45,32 +47,39 @@ export default function HomeLayout({children,}: {children: React.ReactNode}) {
                 </div>
 
                 <nav className="flex-1 lg:flex hidden justify-center items-center gap-x-14 xl:text-2xl lg:text-lg text-md font-semibold">
-                    <Link href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>
+                    {loggedIn && <Link href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>}
                     <Link href={"/random"} className={`${path === "/random" ? "text-primary" : ""}`}>Random Bev</Link>
                     <Link href={"/browse"} className={`${path === "/browse" ? "text-primary" : ""}`}>Browse</Link>
                     <Link href={"/top"} className={`${path === "/top" ? "text-primary" : ""}`}>Top Rated</Link>
-                    <Link href={"/create"} className={`${path === "/create" ? "text-primary" : ""}`}>Create</Link>
+                    {loggedIn &&<Link href={"/create"} className={`${path === "/create" ? "text-primary" : ""}`}>Create</Link>}
                 </nav>
 
-                <Button onClick={handleLogOut} className="lg:flex hidden bg-red-500 font-semibold xl:text-lg lg:px-5 px-2 lg:py-3 py-2 text-md hover:cursor-pointer" variant={"destructive"} type="button">Logout</Button>
+                {loggedIn ? <Button onClick={handleLogOut} className="lg:flex hidden bg-red-500 font-semibold xl:text-xl lg:px-5 px-2 lg:py-3 py-2 text-md hover:cursor-pointer" variant={"destructive"} type="button">Logout</Button> : 
+                <Button onClick={() => router.push("/")} className="lg:flex hidden bg-green-700 font-semibold xl:text-xl lg:px-5 px-2 lg:py-3 py-2 text-md hover:cursor-pointer" variant={"destructive"} type="button">Log in</Button>}
+                
                 <Button onClick={() => setDisplayMenu(prev => !prev)} variant={"default"} className="lg:hidden flex flex-col py-0 px-0 justify-center items-center gap-y-1.5 cursor-pointer">
-                    <div className="bg-white w-8 h-1 rounded-md" />
-                    <div className="bg-white w-8 h-1 rounded-md" />
-                    <div className="bg-white w-8 h-1 rounded-md" />
+                    {displayMenu ? <X size={30} strokeWidth={3}/> : 
+                    <>
+                        <div className="bg-white w-8 h-1 rounded-md" />
+                        <div className="bg-white w-8 h-1 rounded-md" />
+                        <div className="bg-white w-8 h-1 rounded-md" />
+                    </>
+                    }
+
                 </Button>
                 
-                <nav className={`lg:hidden absolute right-3.5 top-15 rounded-lg z-10 p-4 bg-gray-900 border-2 border-blue-300 flex sm:w-[185px] flex-col gap-y-3 text-2xl font-semibold ${displayMenu ? "flex" : "hidden"}`}>
-                    <Link onClick={() => setDisplayMenu(false)} href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>
+                <nav className={`lg:hidden absolute right-3.5 top-15 rounded-lg z-10 p-4 bg-gray-900 border-2 border-blue-300 flex sm:w-[225px] w-[200px] flex-col gap-y-3 md:text-2xl text-xl font-semibold ${displayMenu ? "flex" : "hidden"}`}>
+                    {loggedIn && <Link onClick={() => setDisplayMenu(false)} href={"/home"} className={`${path === "/home" ? "text-primary" : ""}`}>Home</Link>}
                     <Link onClick={() => setDisplayMenu(false)} href={"/random"} className={`${path === "/random" ? "text-primary" : ""}`}>Random Bev</Link>
                     <Link onClick={() => setDisplayMenu(false)} href={"/browse"} className={`${path === "/browse" ? "text-primary" : ""}`}>Browse</Link>
                     <Link onClick={() => setDisplayMenu(false)} href={"/top"} className={`${path === "/top" ? "text-primary" : ""}`}>Top Rated</Link>
-                    <Link onClick={() => setDisplayMenu(false)} href={"/create"} className={`${path === "/create" ? "text-primary" : ""}`}>Create</Link>
-                    <Button onClick={handleLogOut} className="flex bg-red-500 font-semibold px-2 py-2 text-lg hover:cursor-pointer" variant={"destructive"} type="button">Logout</Button>
+                    {loggedIn && <Link onClick={() => setDisplayMenu(false)} href={"/create"} className={`${path === "/create" ? "text-primary" : ""}`}>Create</Link>}
+                    {loggedIn ? <Button onClick={handleLogOut} className="flex bg-red-500 font-semibold px-2 py-2 text-lg hover:cursor-pointer" variant={"destructive"} type="button">Logout</Button> : <Button onClick={() => router.push("/")} className="flex bg-green-700 font-semibold px-2 py-2 text-lg hover:cursor-pointer" variant={"destructive"} type="button">Log in</Button>}
                 </nav>
             </header>
 
             <main className="p-6">{children}</main>
-            <Toaster/>
+            <Toaster position="top-center"/>
         </div>
     )
 }
