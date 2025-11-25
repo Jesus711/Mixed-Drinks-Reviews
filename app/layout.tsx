417b0c504d90c5a601a1fcf09727e6805aeb1b36
Jesus Ruelas <jruelasperez711@gmail.com>
2025-11-25 14:10:15 -0800

    Added linear gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-800`}
+        className={`${geistSans.variable} ${geistMono.variable} relative antialiased bg-linear-to-t from-slate-900 to-slate-800`}
       >
         {children}
       </body>
